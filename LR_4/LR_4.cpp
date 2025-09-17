587910ecce9e574c5b7385430cb09b2832e0eaa2
r0fys <rofysyt@gmail.com>
2025-09-17 22:21:15 +0300

    lab_3.2

---
+++ LR_4/LR_4.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double x; // вхідний аргумент
+    double a; // вхідний параметр
+    double b; // вхідний параметр
+    double c; // вхідний параметр
+    double F; // результат обчислення виразу
+    cout << "a = "; cin >> a;
+    cout << "b = "; cin >> b;
+    cout << "c = "; cin >> c; 
+    cout << "x = "; cin >> x;
+    // спосіб 1: розгалуження в скороченій формі
+    if (x < 0 && b != 0)
+        F = a*pow(x,2)+b;
+    if (x > 0 && b == 0)
+        F = (x - a) / (x - c);
+    if (!(x < 0 && b != 0) && !(x > 0 && b == 0))
+        F = x/c;
+
+    cout << endl;
+    cout << "1) F = " << F << endl;
+
+    // спосіб 2: розгалуження в повній формі
+    if (x < 0 && b != 0)
+        F = F = a * pow(x, 2) + b;
+    else if (x > 0 && b == 0)
+        F = (x - a) / (x - c);
+    else F = x / c;
+          
+    cout << "2) F = " << F << endl;
+
+    return 0;
+}
